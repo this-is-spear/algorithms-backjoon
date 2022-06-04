25432ac1d790d7ae382d14ae5d6c771c8987c51e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-06-05 00:02:05 +0900

    solved 12738

---
+++ solved/12738.py
@@ -0,0 +1,18 @@
+import sys
+from bisect import bisect_left
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().rsplit()))
+lis = [a[0]]
+ans = 1
+
+for num in a[1:]:
+    if lis[-1] < num:
+        lis.append(num)
+        ans += 1
+    else:
+        index = bisect_left(lis, num)
+        lis[index] = num
+print(ans)
